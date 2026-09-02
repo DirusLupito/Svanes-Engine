5a85c4c0b4a01c01478f25000c3db3d89a721876
ErikSvanes <116416661+ErikSvanes@users.noreply.github.com>
2026-09-02 11:24:08 -0400

    added comments to sprite animation and registry header files

--- include/svanes/registry.hpp
+++ include/svanes/registry.hpp
@@ -12,6 +12,8 @@ namespace svanes {
 /**
 * The entity component system registry. This class manages entities and their components.
 * 
+* Read through entity_components.md for a more detailed explanation of how components work with entities.
+* 
 * It allows for creation and destruction of entities, as well as adding, removing, and 
 * querying components associated with those entities.
 * 
@@ -22,7 +24,27 @@ public:
     void DestroyEntity(Entity entity);
 
     /**
+	* Adds a component of type T to the specified entity. If the entity already has a component of that type, it will be replaced.
     * 
+	* Gets a reference to where the component will live in the registry, writes the new component to that location and passes in
+	* the provided arguments to the component's constructor, and returns a reference to the newly added component
+    * 
+    * Example usage:
+    * 
+    * registry.AddComponent<svanes::Transform>(entity);
+    * 
+    * registry.AddComponent<svanes::SpriteAnimation>(entity, svanes::SpriteAnimation{
+    *     .texture = orb_texture,
+    *     .frame_width = 128,
+    *     .frame_height = 128,
+    *     .frame_count = 4,
+    *     .seconds_per_frame = 0.12F,
+    * });
+    * 
+    * @tparam T The component to add.
+    * @param entity The entity to add the component to.
+    * @param args The arguments to pass to the component's constructor.
+    * @return A reference to the added component.
     */
     template <typename T, typename... Args>
     T& AddComponent(Entity entity, Args&&... args)
@@ -32,6 +54,14 @@ public:
         return std::any_cast<T&>(slot);
     }
 
+    /**
+	* Removes a component from the given entity. 
+    * 
+    * If the entity does not have a component of that type, this function is a no-op.
+    * 
+    * @tparam T The component to remove.
+	* @param entity The entity to remove the component from.
+    */
     template <typename T>
     void RemoveComponent(Entity entity)
     {
@@ -41,6 +71,13 @@ public:
         }
     }
 
+    /**
+	* Checks if the given entity has a component of the given type.
+    * 
+    * @tparam T The component to find.
+    * @param entity The entity to check.
+    * @return true if the entity has the component, false otherwise.
+    */
     template <typename T>
     bool HasComponent(Entity entity) const
     {
@@ -52,12 +89,26 @@ public:
         return entity_it->second.contains(std::type_index{typeid(T)});
     }
 
+    /**
+	* Gets a reference to the component of the given type associated with the given entity.
+    * 
+    * @tparam T The component to get.
+    * @param entity The entity to check.
+    * @return A reference to the component.
+    */
     template <typename T>
     T& GetComponent(Entity entity)
     {
         return std::any_cast<T&>(components.at(entity).at(std::type_index{typeid(T)}));
     }
 
+    /**
+	* Iterates over all entities that have all of the specified components.
+    *
+    * @tparam Components The component types to check for.
+    * @tparam Func The type of the function to call for each entity.
+    * @param func The function to call for each entity.
+    */
     template <typename... Components, typename Func>
     void ForEach(Func&& func)
     {
@@ -71,7 +122,10 @@ public:
     }
 
 private:
+	// The unique ID for each entity, which is incremented each time a new entity is created.
     Entity next_entity = 0;
+    
+    // A 2D map of all components in the registry, first indexed by entity and then component type.
     std::unordered_map<Entity, std::unordered_map<std::type_index, std::any>> components;
 };
 
---
+++ src/entity_components.md
@@ -0,0 +1,44 @@
+## Entity Components
+
+In our engine, our entities are composed of various components that define their behavior and properties.
+
+This is opposed to a traditional inheritance-based approach, where entities would inherit from a base class and have their behavior defined by that class hierarchy.
+
+### Components
+
+Components themselves are data structs, and nothing more. They do not contain any logic or behavior.
+Their logic lies in utility functions specific to that component type, which operate on the data contained within the component.
+
+### What this looks like
+
+Creating a component is simply defining a struct with the data you want to store.
+For example the [SpriteAnimation](../include/svanes/sprite_animation_system.hpp) component we have is defined like this:
+
+```cpp
+struct SpriteAnimation {
+    SDL_Texture* texture = nullptr;
+    int frame_width = 0;
+    int frame_height = 0;
+    int frame_count = 1;
+    int current_frame = 0;
+    float seconds_per_frame = 0.1F;
+    float elapsed_seconds = 0.0F;
+};
+```
+
+Like stated above, this struct only contains the data that we want to store and action on.
+
+The logic for this component is contained in a utility function that operates on the data contained within the struct.
+For example the [AdvanceSpriteAnimations](sprite_animation_system.cpp) function is roughly defined like this:
+
+```cpp
+void AdvanceSpriteAnimations(Registry& registry, float delta_seconds)
+{
+    // Run a ForEach on our registry to get every entity that has a <SpriteAnimation> component.
+    registry.ForEach<SpriteAnimation>([delta_seconds](Entity /*entity*/, SpriteAnimation& animation) {
+        // Return early when there is only one frame or there are '0' seconds per frame.
+
+        // Accumulate the elapsed_seconds and check if we need to advance the frame.
+    });
+}
+```
